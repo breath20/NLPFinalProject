cc00630d6a66cba2e8549cbf54f30e38e48df36c
Ryan Swanson <ryan.a.swanson@gmail.com>
2011-10-29 16:48:20 -0600

    Adding part of speech tagger.  Likely won't need the wordnet api afterall.

--- src/nlpfinalproject/NLPFinalProject.java
+++ src/nlpfinalproject/NLPFinalProject.java
@@ -4,6 +4,16 @@
  */
 package nlpfinalproject;
 
+import edu.stanford.nlp.ling.HasWord;
+import edu.stanford.nlp.ling.Sentence;
+import edu.stanford.nlp.ling.TaggedWord;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author ryan
@@ -13,7 +23,17 @@ public class NLPFinalProject {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException, ClassNotFoundException {      
+        if (args.length != 2) {
+            System.err.println("usage: java TaggerDemo modelFile fileToTag");
+            return;
+        }
+        MaxentTagger tagger = new MaxentTagger(args[0]);
+        @SuppressWarnings("unchecked")
+        List<List<HasWord>> sentences = tagger.tokenizeText(new BufferedReader(new FileReader(args[1])));
+        for (List<HasWord> sentence : sentences) {
+            ArrayList<TaggedWord> tSentence = tagger.tagSentence(sentence);
+            System.out.println(Sentence.listToString(tSentence, false));
+        }
     }
 }
